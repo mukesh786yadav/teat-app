d6fc5a3541a39439528ee639d56a26fbe6e67e2d
mukesh786yadav <1032190391@tcetmumbai.in>
2024-03-20 13:22:57 +0530

    env error

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,20 +6,15 @@ import { Container, Row, Col } from "react-bootstrap";
 import Pagination from "../../Components/Pagination/Pagination";
 
 const Home = () => {
-<<<<<<< HEAD
   //const Api_key = "c45a857c193f6302f2b5061c3b85e743";
   const Api_key = "c45a857c193f6302f2b5061c3b85e743";
-=======
-  const Api_key = "c45a857c193f6302f2b5061c3b85e743";
-  //const Api_key = process.env.REACT_APP_API_KEY;
->>>>>>> 467ccb722af7f7de044a14ad7f98b84e37b595ca
   const [movieData, setMovieData] = useState([]);
   const [pageno, setPageno] = useState(1);
   const [pagenationno, setPagenationno] = useState();
 
   const getMoviedata = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${Api_key}&language=en-US&page=${pageno}`,
+      `https://api.themoviedb.org/3/movie/popular?api_key=${Api_key}&language=en-US&page=${pageno}`
     );
     setMovieData(data.results);
     setPagenationno(data.total_pages); // Set total pages for pagination
@@ -43,10 +38,7 @@ const Home = () => {
               </Col>
             ))
           ) : (
-            <div className="text-white text-2xl p-7 justify-items-center">
-              Loading.......
-            </div>
-          )}
+              <div className="text-white text-2xl p-7 justify-items-center">Loading.......</div>          )}
         </Row>
         {pagenationno && pagenationno > 1 && (
           <Pagination
